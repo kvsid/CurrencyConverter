93461abee1f7660f71436f06976c26b8badeb669
kvsid <nice.curly2013@gmail.com>
2020-12-01 15:20:51 +0200

    Add PickerView

--- Converter/ConverterViewController.swift
+++ Converter/ConverterViewController.swift
@@ -8,48 +8,44 @@
 import UIKit
 
 class ConverterViewController: UIViewController {
+    var currencies = [CurrencyInformation(cc: "AUD", rate: 20.9881), CurrencyInformation(cc: "CAD", rate: 21.9061)]
+
+    let backButton : UIBarButtonItem = {
+        let button = UIBarButtonItem()
+        button.title = "Back"
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.title = "Converter"
-//
-        let backButton = UIBarButtonItem()
-        backButton.title = "Back"
-        self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
+        title = "Converter"
+        navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
 
-//        createCurrencyView()
+        let currencySelector = UIPickerView(frame: CGRect(x: 0, y: 200, width: view.frame.width, height: 200))
+        currencySelector.center = view.center
+        currencySelector.dataSource = self
+        currencySelector.delegate = self
+        view.addSubview(currencySelector)
+    }
+}
 
-//        let currencySelector = UIPickerView()
-//        currencySelector.frame = CGRect(x: 0, y: 200, width: self.view.frame.width, height: 200)
-//        view.addSubview(currencySelector)
-//
-//        let secondCurrencyView = UIView()
-//        secondCurrencyView.frame = CGRect(x: 0, y: 400, width: self.view.frame.width, height: 100)
-//        secondCurrencyView.backgroundColor = .systemYellow
-//        view.addSubview(secondCurrencyView)
-
+extension ConverterViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
 
-//    func createCurrencyView() {
-//        let currencyView = UIView()
-//        currencyView.frame = CGRect(x: 0, y: 100, width: self.view.frame.width, height: 100)
-//        currencyView.backgroundColor = .systemYellow
-//        view.addSubview(currencyView)
-//
-//        func createCurrencyField() {
-//            let currencyField = UITextField()
-////                currencyField.size
-//            currencyView.addSubview(currencyField)
-//        }
-//    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencies.count
+    }
+}
 
-//    override func viewDidAppear(_ animated: Bool) {
-//        super.viewDidAppear(true)
-//        self.perform(#selector(goBack), with: nil, afterDelay: 3.0)
- //   }
-//
-//    @objc func goBack() {
-//        self.navigationController?.popViewController(animated: true)
-//    }
+extension ConverterViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencies[row].cc
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("row \(row). component \(component)")
+    }
 }
