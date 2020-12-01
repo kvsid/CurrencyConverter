2d1078bbed59636403320d22e71a4ebf1907adaa
kvsid <nice.curly2013@gmail.com>
2020-12-01 23:38:33 +0200

    Cleanup CurrencyViewController.

--- Converter/CurrencyViewController.swift
+++ Converter/CurrencyViewController.swift
@@ -7,8 +7,52 @@
 
 import UIKit
 
-class CurrencyViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class CurrencyViewController: UIViewController, UITableViewDataSource {
+    var displayConverterButton = UIButton()
+    var currencyTable = UITableView()
+    var currencies = [CurrencyInformation(cc: "AUD", rate: 20.9881), CurrencyInformation(cc: "CAD", rate: 21.9061)]
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Currency"
+        setupTable()
+        setupButton()
+    }
+
+    @objc func performDisplayConverter (parametrSender: Any) {
+        let converter = ConverterViewController()
+        self.navigationController?.pushViewController(converter, animated: true)
+    }
+
+    func setupButton(){
+        displayConverterButton = UIButton(type: .system)
+        displayConverterButton.setTitle("Converter", for: .normal)
+        displayConverterButton.sizeToFit()
+        displayConverterButton.backgroundColor = .systemYellow
+        displayConverterButton.tintColor = .black
+        displayConverterButton
+            .frame = CGRect(x: 0, y: self.view.frame.height - 100, width: self.view.frame.width, height: 100)
+
+
+        displayConverterButton
+            .addTarget(self, action: #selector(performDisplayConverter(parametrSender:)), for: .touchUpInside)
+        view.addSubview(self.displayConverterButton)
+    }
+
+    func setupTable() {
+        currencyTable = UITableView(frame: view.bounds, style: .plain)
+        currencyTable.delegate = self
+        currencyTable.dataSource = self
+        currencyTable.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        currencyTable.register(Cell.self, forCellReuseIdentifier: "Cell")
+
+        view.addSubview(currencyTable)
+    }
+
+}
+
+extension CurrencyViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return currencies.count
     }
@@ -20,46 +64,4 @@ class CurrencyViewController: UIViewController, UITableViewDelegate, UITableView
         cell.currencyRate.text = String(currentCurrency.rate)
         return cell
     }
-
-    var displayConverterButton = UIButton()
-    var currencyTable = UITableView()
-    var currencies = [CurrencyInformation(cc: "AUD", rate: 20.9881), CurrencyInformation(cc: "CAD", rate: 21.9061)]
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        createTable()
-
-        self.title = "Currency"
-
-        self.displayConverterButton = UIButton(type: .system)
-        self.displayConverterButton.setTitle("Converter", for: .normal)
-
-        self.displayConverterButton.sizeToFit()
-        self.displayConverterButton.backgroundColor = .systemYellow
-        self.displayConverterButton.tintColor = .black
-        self.displayConverterButton.frame = CGRect(x: 0, y: self.view.frame.height - 100, width: self.view.frame.width, height: 100)
-
-
-        self.displayConverterButton.addTarget(self, action: #selector(performDisplayConverter(parametrSender:)), for: .touchUpInside)
-        self.view.addSubview(self.displayConverterButton)
-    }
-
-    @objc func performDisplayConverter (parametrSender: Any) {
-        let converter = ConverterViewController()
-        self.navigationController?.pushViewController(converter, animated: true)
-    }
-
-    func createTable() {
-        self.currencyTable = UITableView(frame: view.bounds, style: .plain)
-        self.currencyTable.delegate = self
-        self.currencyTable.dataSource = self
-
-        self.currencyTable.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        self.currencyTable.register(Cell.self, forCellReuseIdentifier: "Cell")
-
-        view.addSubview(currencyTable)
-    }
-
 }
-
